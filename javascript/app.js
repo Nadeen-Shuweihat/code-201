11202c74714bd29120fe97296b5c6500c449007d
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-04-04 18:41:53 +0300

    new website

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let userName = prompt('what is your name?');
+console.log(typeof userName, userName);
+
+
+let userAge = prompt('How old are you?');
+console.log(typeof userAge, userAge);
+
+let userStory = prompt('what is your favorite story as a kid?');
+console.log(typeof userStory, userStory);
+
+let userSong = prompt('what is your favorite song')
+console.log(typeof userSong, userSong)
+
+alert(' Hello ' + userName + ' you are ' + userAge + ' your favorite story as a kid is ' + userStory + ' and your favorite song is '+ userSong);
